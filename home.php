358d8d5722d2f3da5b1fe94b9e767d382112fe63
Mauro Walker <mauro.walker@me.com>
2016-11-14 14:17:36 +0100

    Login angepasst

--- home.php
+++ home.php
@@ -62,7 +62,7 @@
         <li><a href="#">Kontakt</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
       </ul>
     </div>
   </div>
