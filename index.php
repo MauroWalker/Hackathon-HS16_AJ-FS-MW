4e77f32c99436b88c89c12ef61b975d320f7d9fa
Mauro Walker <mauro.walker@me.com>
2016-11-14 14:01:04 +0100

    home.php added

--- index.php
+++ index.php
@@ -57,9 +57,6 @@
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Meine Reisen</a></li>
-        <li><a href="#">Profil</a></li>
-        <li><a href="#">Kontakt</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
@@ -71,9 +68,6 @@
 <div class="container-fluid text-center">
   <div class="row content">
     <div class="col-sm-2 sidenav hidden-xs">
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
     </div>
     
     <div class="col-sm-8 text-left">
