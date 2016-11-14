49f5387ccef4070676843e6c8c6f4a5b5f81c98d
Alesch Jufer <aleschsilvan.jufer@mmp.htwchur.ch>
2016-11-14 15:07:29 +0100

    Data updated

--- system/data.php
+++ system/data.php
@@ -2,7 +2,7 @@
 
   function get_db_connection()
   {
-    $db = mysqli_connect('localhost', '546281_2_1', 'z7bxtK0RT162', '546281_2_1')
+    $db = mysqli_connect('localhost', '260978_3_1', 'gQZTGEbRdq8s', '260978_3_1')
       or die('Fehler beim Verbinden mit dem Datenbank-Server.');
       mysqli_set_charset($db, "utf8");
     return $db;
@@ -24,12 +24,12 @@
 
   function login($email, $password)
   {
-    $sql = "SELECT * FROM user WHERE email = '$email' AND password = '$password';";
+    $sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password';";
     return get_result($sql);
   }
 
   function register($email , $password){
-    $sql = "INSERT INTO user (email, password) VALUES ('$email', '$password');";
+    $sql = "INSERT INTO user (email, password, confirm-password, email, plz, ort, ga) VALUES ('$username', '$password', '$confirm-password' , '$email', "$plz", "$ga");";
 		return get_result($sql);
 	}
 
