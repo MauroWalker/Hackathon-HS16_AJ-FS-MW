542c667e62dfa5ab9dadab41a9cfd9f303a4bca9
Alesch Jufer <aleschsilvan.jufer@mmp.htwchur.ch>
2016-11-24 15:22:13 +0100

    no message

--- about.php
+++ about.php
@@ -10,6 +10,23 @@
 	require_once("system/data.php");
 	require_once("system/security.php");
 
+
+
+  if(isset($_POST['update-submit'])){
+    $email = filter_data($_POST['email']);
+    $password = filter_data($_POST['password']);
+    $confirm_password = filter_data($_POST['confirm_password']);
+    $username = filter_data($_POST['username']);
+    $plz = filter_data($_POST['ort']);
+    $ort = filter_data($_POST['plz']);
+
+    $result = update_user($user_ID, $email, $password, $confirm_password, $username, $plz, $ort);
+  }
+
+
+  $result = get_user($user_ID);
+  $user = mysqli_fetch_assoc($result);
+  
 ?>
 
 <html lang="en">
@@ -67,6 +84,77 @@ Bock auf Reisen LLC ist nicht verantwortlich für den Inhalt externer Websites. 
     </label>
   </form>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <form enctype="multipart/form-data" action="<?PHP echo $_SERVER['PHP_SELF'] ?>" method="post">
+        <div class="modal-header">
+          <h4 class="modal-title" id="myModalLabel">Profileinstellungen der UserID#<?PHP echo $user_ID?></h4>
+        </div>
+        <div class="modal-body">
+
+          <div class="form-group row">
+            <label for="Username" class="col-sm-2 col-xs-12 form-control-label">Username</label>
+            <div class="col-sm-5 col-xs-6">
+              <input  type="text" class="form-control form-control-sm"
+                      id="username" placeholder="Username"
+                      name="username" value="<?php echo $user['username']; ?>">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="Email" class="col-sm-2 form-control-label">E-Mail</label>
+            <div class="col-sm-10">
+              <input  type="email" class="form-control form-control-sm"
+                      id="Email" placeholder="E-Mail"
+                      name="email" value="<?php echo $user['email']; ?>">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="Passwort" class="col-sm-2 form-control-label">Password</label>
+            <div class="col-sm-10">
+              <input type="password" class="form-control form-control-sm" id="Passwort" placeholder="Passwort" name="password">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="Passwort_Conf" class="col-sm-2 form-control-label">Passwort bestätigen</label>
+            <div class="col-sm-10">
+              <input type="password" class="form-control form-control-sm" id="Passwort_Conf" placeholder="Passwort" name="confirm_password">
+            </div>
+          </div>
+
+
+                  <div class="form-group row">
+            <label for="Passwort_Conf" class="col-sm-2 form-control-label">PLZ</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control form-control-sm" id="plz1" placeholder="PLZ" name="plz" value="<?php echo $user['plz']; ?>">
+            </div>
+          </div>
+        </div>
+
+                  <div class="form-group row">
+            <label for="Passwort_Conf" class="col-sm-2 form-control-label">Ort</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control form-control-sm" id="ort1" placeholder="Ort" name="ort" value="<?php echo $user['ort']; ?>"
+            </div>
+          </div>
+
+
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Abbrechen</button>
+          <button type="submit" class="btn btn-success btn-sm" name="update-submit">Änderungen speichern</button>
+        </div>
+      </form>
+</div>
+    </div>
+  </div>
+</div>
+<!-- Modal -->
+
+
 
 <?php include ("footer.php"); ?>
 </body>
